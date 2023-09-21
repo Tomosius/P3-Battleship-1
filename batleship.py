5dd18f6bc1590de7e5de1640a0bea72543e6d0e9
Tomosius <pecukevicius@gmail.com>
2023-09-21 14:41:21 +0100

    rewriting game as came with better ideas to search

--- batleship.py
+++ batleship.py
@@ -1,9 +1,9 @@
 """
 Section for Global Variables
 """
-X = 10           # default map size X - horizontal
-Y = 10          # default map size Y - vertical
-mapSize = [X,Y]   # first number - horizontal lines, second - vertical, by default it will be 10 x 10
+mapX = 10           # default map size X - horizontal
+mapY = 10          # default map size Y - vertical
+
 
 """
 default Battleship fleet - i use as dictionary, so in case if player changes map size or ships quantities, it will be easier to play
@@ -11,33 +11,44 @@ default Battleship fleet - i use as dictionary, so in case if player changes map
 battleshipFleet = {
     "Aircraft Carrier": {
         "Size": 5,
-        "Quantity": 3
+        "Quantity": 1
     },
     "Battleship": {
         "Size": 4,
-        "Quantity": 2
+        "Quantity": 0
     },
     "Cruiser": {
         "Size": 3,
-        "Quantity": 3
+        "Quantity": 0
     },
     "Submarine": {
         "Size": 3,
-        "Quantity": 2
+        "Quantity": 0
     },
     "Destroyer": {
         "Size": 2,
-        "Quantity": 3
+        "Quantity": 0
     },
-    "tomas": {
+    "Dingy Boat": {
         "Size": 1,
-        "Quantity": 3
+        "Quantity": 0
     }
 }
 
 """
+function to print out all fleet as a table
+"""
+def printFleet():
+    print("{:<20} {:<10} {:<10}".format("Ship Type", "Size", "Quantity"))
+    print("="*40)
+    for ship, details in battleshipFleet.items():
+        size = details["Size"]
+        quantity = details["Quantity"]
+        print("{:<20} {:<10} {:<10}".format(ship, size, quantity)) 
+"""
 will create list of colors, so i can use them on map.
 """
+
 colors = {
     "DARK_YELLOW": "\033[33m",   # Dark Yellow - single cell ship
     "LIGHT_GRAY": "\033[37m",    # Light Gray - empty space on map,. n o need to shoot
@@ -61,109 +72,42 @@ def mapSizeSelect(): #function for user to input map size
         # user will be requested to input map size
         mapSize = input("Please select MAP size You would like to play. First number - Width, second number - Height. Example: 10,10 \n")
         try: #testing if user used correct pattern to input map size
-            X,Y = mapSize.split(',') # splitting user input into 2 separate parts 
-            if X.isdigit() and Y.isdigit(): # checking if both inputs parts are numbers
+            mapX,mapY = mapSize.split(',') # splitting user input into 2 separate parts 
+            if mapX.isdigit() and mapY.isdigit(): # checking if both inputs parts are numbers
                 mapSize = [int(X), int(Y)] #if both inputs are valid, assigning them to mapSize
                 break #now function can stop as all is correct
             else:  #if any part of inout is not numeric, printing out message
                 print("Invalid input. Please enter two numbers separated by a comma.")
         except ValueError: #if user input can not be split in 2 parts will raise an issue:
             print("Invalid input. Please enter two numbers separated by a comma.")
-    print(mapSize)
-
-
-"""
-function to start new Game
-"""
-def newGame(): #function to start new game
-    mapSizeSelect() #asking user to input new map size
-
+        return mapX,mapY
 
 """
 creating an array that will be representing map based on X and Y
 """
-mapCPU = [[0 for c in range(Y)] for r in range(X)]
-mapPlayer = [[0 for j in range(Y)] for i in range(X)]
+mapCPU = [[0 for c in range(mapY)] for r in range(mapX)]
+mapPlayer = [[0 for j in range(mapY)] for i in range(mapX)]
 
 
 """
 Print the CPU MAP array to the console - will create function, later will be easier to print the map
 """
-def printMap(printMap):
-    for row in printMap:
-        for item in row:
-            print(item, end="")
-        print()  
+def printMap(table):
+    print("   ", end="")
+    for col_index in range(len(table[0])):
+        print(f"{col_index}  ", end="")
+    print("\n" + "   " + "=" * (len(table[0]) * 3))
 
-
-"""
-will rotate map, so colmns becomes rows, rows into columns. This will help to use same function to search on map, but now it would be searching in columns, after such search, map will need to be fliped again back to notmal state
-"""
-def mapFlip(mapRotate):
-    return [[mapRotate[j][i] for j in range(len(mapRotate))] for i in range(len(mapRotate[0]))]
-
-
+    # Iterate through the table and print row coordinates on the left side
+    for row_index, row in enumerate(table):
+        print(f"{row_index} |", end=" ")
+        for value in row:
+            print(f"{value}  ", end="")
+        print() 
 
 """
-creating loop for all battleships, starting name and then going to sublevel - QTY of each of them
+function to create ship or pattern by given width and length, will be hande searching map
 """
-def deployAllShips():
-    global mapCPU  # Add this line
-    global battleshipFleet
-    for shipName, shipInfo in battleshipFleet.items():
-        quantity = shipInfo["Quantity"]
-        size = shipInfo["Size"]
-        print(f"Deploying {quantity} {shipName}(s) of size {size}")
-        for i in range(quantity):
-            align = random.choice(["horizontal", "vertical"])
-            symbol = 'V' if align == 'vertical' else 'H'
-            mapSearch = mapFlip(mapCPU) if align == 'vertical' else mapCPU
-            location = findShipLocation(mapSearch, size, 0)
-            if location is not None:
-                shipDeploy(mapSearch, size, location, symbol)
-                mapCPU = mapFlip(mapSearch) if align == 'vertical' else mapSearch
-            else:
-                print(f"Error: Cannot deploy {shipName}. No valid location found.")
-            mapCPU = mapFlip(mapSearch) if align == 'vertical' else mapSearch
+def createPattern(width, height):
+    return [[0] * width for _ in range(height)]
 
-
-"""
-function to find given ship on map (search by rows), if needed to search by columns, searchMap columns should be converted to rows and rows to columns, use function mapConvertRowsToColumns(map)
-"""
-def findShipLocation(searchMap, shipLength, cellValue):
-    validLocations = []  # List to store valid ship deployment locations
-    mapSizeX = len(searchMap[0])
-    mapSizeY = len(searchMap)
-    for c in range(mapSizeY):  # Loop through all columns
-        for r in range(mapSizeX - shipLength + 1):  # Loop through row cells
-            if all(cell == cellValue for cell in searchMap[c][r:r + shipLength]):
-                # Check if the ship can fit in the row
-                validLocations.append((c, r))  # Store valid location as (row, column)
-    if validLocations:
-        return random.choice(validLocations)  # Choose a random valid location
-    else:
-        return None  # No valid location found for the ship
-
-
-"""
-function to deploy ship to map by given location, and ships will be displayed as symbols not letters, so it is easier to see where is what ship, is it vertical or horizontal and etc.
-"""
-def shipDeploy(map, shipLength, location, symbol):
-    row, column = location  # getting row and column numbers
-    
-    if shipLength == 1: # if ship ius made just of one cell, then we will show only:
-        color = colors["DARK_YELLOW"]
-        map[row][column] = color +  chr(0x25C6) + colors["RESET"]# ship will be displayed as ◆
-    else:
-        if symbol == "H": 
-            color = colors["DARK_BLUE"]
-            map[row][column] = color + chr(0x25C0) + colors["RESET"]# if ship is horizontal, first cell will be ◂
-        else:
-            color = colors["DARK_GREEN"]
-            map[row][column] = color + chr(0x25B2) + colors["RESET"]# if ship is vertical, first cell will be ▲
-        column = column + 1
-        for i in range(shipLength - 1):  # looping through given location
-            map[row][column + i] = color + chr(0x25A0)  + colors["RESET"]# deploying ship all remaining cells as ■
-
-deployAllShips()
-printMap(mapCPU)
